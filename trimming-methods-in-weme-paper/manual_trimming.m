48f76a09ae8a70a6accaa8d3040c80bf802286e3
MohammedTwheed <MohammedTwheed@gmail.com>
2024-06-12 23:31:36 +0300

    before adding the stopping criterial for the main for loop

---
+++ trimming-methods-in-weme-paper/manual_trimming.m
@@ -0,0 +1,5 @@
+% according to "Prediction of the Effect of Impeller Trimming on the 
+% Hydraulic Performance of Low Specific-Speed Centrifugal Pumps" 
+% paper of D. G. J. Detert Oude Weme et al.
+% they discused mainly 
+
