a56de32a4eaac425aa5f09278dc154ab5cfa9c51
MohammedTwheed <MohammedTwheed@gmail.com>
2024-06-26 18:00:45 +0300

    now results is displayed as table and saved to csv only for QHD and QDP, but there is issue with 220 diameter

--- explorations/loop_05/main_02.m
+++ explorations/loop_05/main_02.m
@@ -1,22 +1,20 @@
-%% 
 clear; clc; clf;
 load('filtered_QHD_table.mat')
 load('filtered_QDP_table.mat')
 load('deleted_QHD_table.mat')
 load('deleted_QDP_table.mat')
 
-
-QH = [filtered_QHD_table.FlowRate_m3h,filtered_QHD_table.Head_m]';
+QH = [filtered_QHD_table.FlowRate_m3h, filtered_QHD_table.Head_m]';
 D  = [filtered_QHD_table.Diameter_mm]';
 
-QH_beps=[deleted_QHD_table.FlowRate_m3h,deleted_QHD_table.Head_m]';
-D_beps=[deleted_QHD_table.Diameter_mm]';
+QH_beps = [deleted_QHD_table.FlowRate_m3h, deleted_QHD_table.Head_m]';
+D_beps = [deleted_QHD_table.Diameter_mm]';
 
-QD = [filtered_QDP_table.FlowRate_m3h,filtered_QDP_table.Diameter_mm]';
+QD = [filtered_QDP_table.FlowRate_m3h, filtered_QDP_table.Diameter_mm]';
 P = [filtered_QDP_table.Power_kW]';
 
-QD_beps=[deleted_QDP_table.FlowRate_m3h,deleted_QDP_table.Diameter_mm]';
-P_beps=[deleted_QDP_table.Power_kW]';
+QD_beps = [deleted_QDP_table.FlowRate_m3h, deleted_QDP_table.Diameter_mm]';
+P_beps = [deleted_QDP_table.Power_kW]';
 
 % these hyper params are based on our latest optimization with ga
 % now we want to finalize our work.
@@ -28,135 +26,93 @@ nn_QDP_size_matrix = [12,15];
 maxEpochs = 191;
 trainFcn= 'trainlm';
 
-[trainedNetQHD,avgMSEsQHD,trainPerformanceQHD,valPerformanceQHD,testPerformanceQHD] = train_nn([2,15,],maxEpochs,trainFcn ,QH, D, randomSeed);
-[trainedNetQDH,avgMSEsQDH,trainPerformanceQDH,valPerformanceQDH,testPerformanceQDH] = train_nn([2,15,],maxEpochs,trainFcn ,[QH(1,:); D], QH(2,:), randomSeed);
-[trainedNetQDP,avgMSEsQDP,trainPerformanceQDP,valPerformanceQDP,testPerformanceQDP] = train_nn([12,15,],maxEpochs,trainFcn ,QD, P, randomSeed);
+% Train on full dataset
+[trainedNetQHD, avgMSEsQHD, trainPerformanceQHD, valPerformanceQHD, testPerformanceQHD] = train_nn([2,15,], maxEpochs, trainFcn, QH, D, randomSeed);
+[trainedNetQDH, avgMSEsQDH, trainPerformanceQDH, valPerformanceQDH, testPerformanceQDH] = train_nn([2,15,], maxEpochs, trainFcn, [QH(1,:); D], QH(2,:), randomSeed);
+[trainedNetQDP, avgMSEsQDP, trainPerformanceQDP, valPerformanceQDP, testPerformanceQDP] = train_nn([12,15,], maxEpochs, trainFcn, QD, P, randomSeed);
+
+% Arrays to save performance metrics
+QHD_results = [];
+QDP_results = [];
 
 %% loop to train on different diameters hidden for QHD dataset
-
-% here is distinctDiameters based on QHD dataset.
 distinctDiametersQHD = unique(D);
-
-
 for dIdx = 1:length(distinctDiametersQHD)
-    % Current diameter to remove
     diameterToRemove = distinctDiametersQHD(dIdx);
-    
-    % Find indices of the current diameter in D
     indicesToRemove = find(D == diameterToRemove);
-    
-    % Store the removed data for later use
     removedQH = QH(:, indicesToRemove);
     removedD = D(indicesToRemove);
-    
-    % Remove rows from QH and D based on the indices
     QH_temp = QH;
     D_temp = D;
     QH_temp(:, indicesToRemove) = [];
     D_temp(:, indicesToRemove) = [];
     
-
-[trainedNetQHD,avgMSEsQHD,trainPerformanceQHD,valPerformanceQHD,testPerformanceQHD] = train_nn([2,15,],maxEpochs,trainFcn ,QH_temp, D_temp, randomSeed);
-[trainedNetQDH,avgMSEsQDH,trainPerformanceQDH,valPerformanceQDH,testPerformanceQDH] = train_nn([2,15,],maxEpochs,trainFcn ,[QH_temp(1,:); D_temp], QH_temp(2,:), randomSeed);
-
-
-
+    [trainedNetQHD_temp, avgMSEsQHD_temp, trainPerformanceQHD_temp, valPerformanceQHD_temp, testPerformanceQHD_temp] = train_nn([2,15,], maxEpochs, trainFcn, QH_temp, D_temp, randomSeed);
+    mse_deleted_diameter = perform(trainedNetQHD_temp, removedD, trainedNetQHD_temp(removedQH));
+    mse_beps = perform(trainedNetQHD_temp, D_beps, trainedNetQHD_temp(QH_beps));
+    
+    QHD_results = [QHD_results; diameterToRemove, avgMSEsQHD_temp, trainPerformanceQHD_temp, valPerformanceQHD_temp, testPerformanceQHD_temp, mse_deleted_diameter, mse_beps];
 end
-
-
 
 %% loop to train on different diameters hidden for QDP dataset
-
-% Get unique diameters for the QDP dataset
 distinctDiametersQDP = unique(QD(2,:));
-
-% Extract the Q and D is already there in QD.
-Q_QDP = QD(1,:);
-D_QDP = QD(2,:);
-
-% Create a structure to hold Q,P curves for each diameter in QDP dataset.
-pump_data_QDP = struct('Diameter', cell(length(distinctDiametersQDP), 1), 'Q', cell(length(distinctDiametersQDP), 1), 'P', cell(length(distinctDiametersQDP), 1));
-
-for i = 1:length(distinctDiametersQDP)
-    idx = (D_QDP == distinctDiametersQDP(i));
-    pump_data_QDP(i).Diameter = distinctDiametersQDP(i);
-    pump_data_QDP(i).Q = Q_QDP(idx);
-    pump_data_QDP(i).P = P(idx);
-end
-
 for dIdx = 1:length(distinctDiametersQDP)
-    % Current diameter to remove
     diameterToRemove = distinctDiametersQDP(dIdx);
-    
-    % Find indices of the current diameter in D_QDP
-    indicesToRemove = find(D_QDP == diameterToRemove);
-    
-    % Store the removed data for later use
+    indicesToRemove = find(QD(2,:) == diameterToRemove);
     removedQD = QD(:, indicesToRemove);
     removedP = P(indicesToRemove);
-    
-    % Remove rows from QD and P based on the indices
     QD_temp = QD;
     P_temp = P;
     QD_temp(:, indicesToRemove) = [];
     P_temp(indicesToRemove) = [];
     
-    [trainedNetQDP, avgMSEsQDP, trainPerformanceQDP, valPerformanceQDP, testPerformanceQDP] = train_nn([12,15,], maxEpochs, trainFcn, QD_temp, P_temp, randomSeed);
+    [trainedNetQDP_temp, avgMSEsQDP_temp, trainPerformanceQDP_temp, valPerformanceQDP_temp, testPerformanceQDP_temp] = train_nn([12,15,], maxEpochs, trainFcn, QD_temp, P_temp, randomSeed);
+    mse_deleted_diameter = perform(trainedNetQDP_temp, removedP, trainedNetQDP_temp(removedQD));
+    mse_beps = perform(trainedNetQDP_temp, P_beps, trainedNetQDP_temp(QD_beps));
+    
+    QDP_results = [QDP_results; diameterToRemove, avgMSEsQDP_temp, trainPerformanceQDP_temp, valPerformanceQDP_temp, testPerformanceQDP_temp, mse_deleted_diameter, mse_beps];
 end
 
+% Save results to CSV
+QHD_results_table = array2table(QHD_results, 'VariableNames', {'Diameter', 'AvgMSEs', 'TrainPerformance', 'ValPerformance', 'TestPerformance', 'MSE_Deleted_Diameter', 'MSE_BEPS'});
+QDP_results_table = array2table(QDP_results, 'VariableNames', {'Diameter', 'AvgMSEs', 'TrainPerformance', 'ValPerformance', 'TestPerformance', 'MSE_Deleted_Diameter', 'MSE_BEPS'});
+
+writetable(QHD_results_table, 'QHD_results.csv');
+writetable(QDP_results_table, 'QDP_results.csv');
+
+% Display results in a table format
+disp('QHD Results:');
+disp(QHD_results_table);
+
+disp('QDP Results:');
+disp(QDP_results_table);
 
 %% functions
 
+function [trainedNet, avgMSEs, trainPerformance, valPerformance, testPerformance] = train_nn(nn_size_matrix, maxEpochs, trainFcn, x, t, randomSeed)
+    rng(randomSeed); % Set random seed for reproducibility.
+    net = feedforwardnet(nn_size_matrix, trainFcn);
+    net.trainParam.showWindow = false;
+    net.trainParam.epochs = maxEpochs;
+    net.performFcn = 'mse';
+    net.input.processFcns = {'removeconstantrows', 'mapminmax'};
+    net.output.processFcns = {'removeconstantrows', 'mapminmax'};
+    net.divideFcn = 'dividerand';  % Divide data randomly
+    net.divideMode = 'sample';  % Divide up every sample
+    net.divideParam.trainRatio = 0.7;
+    net.divideParam.valRatio = 0.15;
+    net.divideParam.testRatio = 0.15;
 
-function [trainedNet,avgMSEs,trainPerformance,valPerformance,testPerformance] = train_nn(nn_size_matrix,maxEpochs,trainFcn ,x, t, randomSeed)
-        rng(randomSeed); % Set random seed for reproducibility.
-        % Define the neural network.
-        net = feedforwardnet(nn_size_matrix,trainFcn);
-        % Suppress training GUI for efficiency.
-        net.trainParam.showWindow = false;
-        net.trainParam.epochs = maxEpochs;
+    [trainedNet, tr] = train(net, x, t);
+    predictions = trainedNet(x);
+    mse = perform(trainedNet, t, predictions);
 
-        % Choose a Performance Function
-        net.performFcn = 'mse';
+    trainTargets = t .* tr.trainMask{1};
+    valTargets = t .* tr.valMask{1};
+    testTargets = t .* tr.testMask{1};
+    trainPerformance = perform(net, trainTargets, predictions);
+    valPerformance = perform(net, valTargets, predictions);
+    testPerformance = perform(net, testTargets, predictions);
 
-        % Choose Input and Output Pre/Post-Processing Functions
-        net.input.processFcns = {'removeconstantrows', 'mapminmax'};
-        net.output.processFcns = {'removeconstantrows', 'mapminmax'};
-
-        % Define data split for training, validation, and testing.
-
-        % For a list of all data division functions type: help nndivide
-        net.divideFcn = 'dividerand';  % Divide data randomly
-        net.divideMode = 'sample';  % Divide up every sample
-        net.divideParam.trainRatio = 0.7;
-        net.divideParam.valRatio = 0.15;
-        net.divideParam.testRatio = 0.15;
-
-
-        % Train the neural network.
-        [trainedNet, tr] = train(net, x, t);
-
-        % Evaluate the model performance using mean squared error (MSE).
-
-        % predictions = trainedNet(normalized_input);
-        predictions = trainedNet(x);
-        mse = perform(trainedNet, t, predictions);
-
-        % Recalculate Training, Validation and Test Performance
-        trainTargets        = t .* tr.trainMask{1};
-        valTargets          = t .* tr.valMask{1};
-        testTargets         = t .* tr.testMask{1};
-        trainPerformance    = perform(net,trainTargets,predictions);
-        valPerformance      = perform(net,valTargets,predictions);
-        testPerformance     = perform(net,testTargets,predictions);
-
-        % for better performance we came up with this convention rather than
-        % using the mse based on perform function only
-        avgMSEs = (mse +  ...
-            trainPerformance +...
-            valPerformance+....
-            testPerformance) / 4;
-        
-        
+    avgMSEs = (mse + trainPerformance + valPerformance + testPerformance) / 4;
 end
-
